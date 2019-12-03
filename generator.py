36c518ca71307c2ec2497e554bd298dec0892491
BCWY <56329073+BCWY@users.noreply.github.com>
2019-12-03 15:45:49 -0800

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,6 @@
+descriptor1 = ["Multiversal", "Singularity", "Chronoplasmic", "Hexagonal", "Unimorphic", "Shamanistic", "Solar", "Self-assembling", "Extradimensional"]
+descriptor2 = ["god", "anti-matter", "null", "rainbow", "dream", "radio"]
+descriptor3 = ["bullet,", "totem,", "particle,", "pill,", "cycle,", "aliens,", "apocalypse,", "midnight,", "plane,"]
+
+import random
+print("Your new plot idea is:", random.choice(descriptor1), random.choice(descriptor2), random.choice(descriptor3), "enjoy!")
